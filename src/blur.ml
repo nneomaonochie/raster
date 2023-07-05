b5ec7d383f6a2641c01fe41fee40397c5eb20ed8
Nneoma <nneoma.amy@gmail.com>
2023-07-05 16:30:48 +0000

    almost done with blur

--- src/blur.ml
+++ src/blur.ml
@@ -1,8 +1,24 @@
 open Core
 
-(* You need to modify this function to blur the input image
-   based on the provided radius instead of ignoring it. *)
-let transform image ~radius:_ = image
+(* takes coordinates and a pixel and blurs the pixel *)
+let transform image ~radius =
+  (* takes coordinates and a pixel and blurs the pixel by taking average of
+     pixels *)
+  let blur_pixels ~x ~y (pix : Pixel.t) =
+    let sub_image =
+      (* you need to acccount for when something is out of bounds yourself *)
+      Image.slice
+        image
+        ~x_start:(x - radius)
+        ~x_end:(x + radius)
+        ~y_start:(y - radius)
+        ~y_end:(y + radius)
+    in
+    ignore pix;
+    Image.mean_pixel sub_image
+  in
+  Image.mapi image ~f:blur_pixels
+;;
 
 let command =
   Command.basic
@@ -24,5 +40,6 @@ let command =
         let image' = transform image ~radius in
         Image.save_ppm
           image'
-          ~filename:(String.chop_suffix_exn filename ~suffix:".ppm" ^ "_blur.ppm")]
+          ~filename:
+            (String.chop_suffix_exn filename ~suffix:".ppm" ^ "_blur.ppm")]
 ;;
