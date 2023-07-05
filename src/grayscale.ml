534dc076581c0ac9e552c00eb3e7a9c20acf39c3
Nneoma <nneoma.amy@gmail.com>
2023-07-05 15:42:49 +0000

    Finished the grayscale function

--- src/grayscale.ml
+++ src/grayscale.ml
@@ -1,9 +1,16 @@
 open Core
 
-(* You need to change the implementation of this function so that it does something
-   to the image instead of just leaving it untouched. *)
+(* transforms an image to be a grayscale version of itself *)
 let transform image =
-  image
+  (* takes in a pixel and returns the pixel in a grayscale tone*)
+  let convert_to_grayscale (pix : Pixel.t) =
+    let gray_value =
+      (Pixel.red pix + Pixel.green pix + Pixel.blue pix) / 3
+    in
+    let new_pix : Pixel.t = gray_value, gray_value, gray_value in
+    new_pix
+  in
+  Image.map image ~f:convert_to_grayscale
 ;;
 
 let command =
@@ -20,5 +27,6 @@ let command =
         let image = Image.load_ppm ~filename |> transform in
         Image.save_ppm
           image
-          ~filename:(String.chop_suffix_exn filename ~suffix:".ppm" ^ "_gray.ppm")]
+          ~filename:
+            (String.chop_suffix_exn filename ~suffix:".ppm" ^ "_gray.ppm")]
 ;;
